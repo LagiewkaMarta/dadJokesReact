1adb5dd8779b913330e8f5ec7a12fe78625c8a73
Marta <lagiewka.marta.dev@gmail.com>
2019-07-13 22:03:04 +0200

    INCLUDE  emoji library in index.html, ADD Joke.css styling

--- src/Joke.js
+++ src/Joke.js
@@ -1,14 +1,16 @@
 import React from "react";
+import "./Joke.css";
 
 export default function Joke({ votes, text, downvote, upvote }) {
   return (
     <div className="Joke">
       <div className="Joke-buttons">
         <i className="fas fa-arrow-up" onClick={upvote}/>
-        <span>{votes}</span>
+        <span className="Joke-votes">{votes}</span>
         <i className="fas fa-arrow-down" onClick={downvote}/>
       </div>
       <div className="Joke-text">{text}</div>
+      <div className="Joke-smiley"><i class="em em-rolling_on_the_floor_laughing"></i></div>
     </div>
   );
 }
